e3ed6f430f6b515bd61a05f4b91b584a694d2af7
Mikita Tkach <Mikita.Tkach@itechart-group.com>
2021-06-10 11:12:48 +0300

    project create

---
+++ CityTransportSystem/CTSCore/Entities/TransportTypeStop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTSCore.Entities
+{
+    class TransportTypeStop
+    {
+        public int Id { get; set; }
+
+        public int TransportTypeId { get; set; }
+        public TransportType TransportType { get; set; }
+
+        public int StopId { get; set; }
+        public Stop Stop { get; set; }
+    }
+}
